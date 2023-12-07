d86de020a91b5b8764cf5e858362a78bcd6cd832
miguelkjesus <miguelkjesus05@gmail.com>
2023-12-07 15:17:21 +0000

    fixed post_to_title_image not working on other machines

--- reddit.py
+++ reddit.py
@@ -1,4 +1,4 @@
-import requests
+import requests, os
 from typing import Literal
 from utils import human_format
 from datetime import datetime, timezone
@@ -13,6 +13,29 @@ def top(subreddit: str, duration: None | Literal["today", "week", "month", "year
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0"
         }
     ).json()
+
+
+async def web_to_image(url: str, path: str = None) -> bytes:
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto(url)
+    width, height = await page.evaluate("() => [document.documentElement.offsetWidth, document.documentElement.offsetHeight]")
+
+    options = {
+        "clip": {
+            "x": 0,
+            "y": 0,
+            "width": width,
+            "height": height
+        },
+        "omitBackground": True
+    }
+    if path is not None:
+        options = options | {"path": path}
+        
+    img = await page.screenshot(options)
+    await browser.close()
+    return img
 
 
 async def post_to_title_image(post: dict, path: str = None) -> bytes | str:
@@ -32,24 +55,4 @@ async def post_to_title_image(post: dict, path: str = None) -> bytes | str:
     with open("./reddit_post_template/index.html", "w") as f:
         f.write(html)
 
-    browser = await launch()
-    page = await browser.newPage()
-    await page.goto("file:///C:/work/projects/reddit-video-generator/reddit_post_template/index.html")
-    width, height = await page.evaluate("() => [document.documentElement.offsetWidth, document.documentElement.offsetHeight]")
-
-    options = {
-        "clip": {
-            "x": 0,
-            "y": 0,
-            "width": width,
-            "height": height
-        },
-        "omitBackground": True
-    }
-    if path is not None:
-        options = options | {"path": path}
-        
-    img = await page.screenshot(options)
-    await browser.close()
-    return img
-
+    await web_to_image(f"file:///{os.path.abspath('./reddit_post_template/index.html')}", path)
