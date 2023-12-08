1873041714998964b6655d50c1fdc2e50b5e3b6f
miguelkjesus <miguelkjesus05@gmail.com>
2023-12-08 23:18:31 +0000

    fixed a bug which produced corrupted files

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ def get_config():
         return yaml.safe_load(f)
 
 
-def crop_video(clip, aspect_ratio: tuple[int, int]):
+def set_aspect_ratio(clip, aspect_ratio: tuple[int, int]):
     w, h = clip.size
     target_width = int(h * aspect_ratio[0] / aspect_ratio[1])
-    return crop(clip, width=target_width, height=h, x_center=w/2, y_center=h/2)
+    if target_width % 2 == 1: 
+        target_width -= 1
+    return crop(clip, width=target_width, height=h, x_center=w//2, y_center=h//2)
 
 
 async def main():
@@ -51,9 +53,8 @@ async def main():
                     await reddit.get_text_image(pg, paragraphPngFilename)
                     pngPaths.append(paragraphPngFilename)
             
-            clip = crop_video(VideoFileClip(random.choice(config["videos"])), (9, 16))
-
-            padding = 20
+            clip = VideoFileClip(random.choice(config["videos"]))
+            clip = set_aspect_ratio(clip, (9, 16))
 
             start = 10
             t = start
@@ -62,23 +63,23 @@ async def main():
                 audio = AudioFileClip(mp3Path)
                 portion = clip.subclip(t, t + audio.duration).set_audio(audio)
                 image = ImageClip(pngPath).set_duration(audio.duration)
-                target_height = image.size[1] * (portion.size[0] - 2 * padding) / image.size[0]
+                target_height = image.size[1] * (portion.size[0] - 40) / image.size[0]
                 image = image.resize(width=portion.size[0], height=target_height)
                 image = image.set_position(((portion.size[0] - image.size[0]) // 2, (portion.size[1] - image.size[1]) // 2))
                 clips.append(CompositeVideoClip([portion, image]).set_start(t - start))
                 t += audio.duration
 
-            # add subscribe clip
-            scale = 0.3
+            # # add subscribe clip
+            scale = 0.4
             subscribe_clip = VideoFileClip("./videos/subscribe.mp4")
             subscribe_clip = (subscribe_clip
                               .resize(width=subscribe_clip.size[0] * scale, height=subscribe_clip.size[1] * scale)
                               .fx(vfx.mask_color, color=[0, 255, 8], thr=180, s=5)
                               .set_start(5))
-            subscribe_clip = subscribe_clip.set_position(((clip.size[0] - subscribe_clip.size[0]) // 2, padding))
+            subscribe_clip = subscribe_clip.set_position(((clip.size[0] - subscribe_clip.size[0]) // 2, 50))
 
             clip = CompositeVideoClip([*clips, subscribe_clip]).subclip(0, min(t, 59))
-            clip.write_videofile(f"{postdir}/video.mp4", fps=30)
+            clip.write_videofile(f"{postdir}/video.mp4")
 
 
 if __name__ == "__main__":
